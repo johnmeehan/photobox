73b9c36283a713a560c376b923df1d407bb71a71
John Meehan <john.meehan1@gmail.com>
2014-06-04 09:03:35 +0100

    Added Gems - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photobox::Application.config.secret_token = 'd6aa6152960a4e6e31a1a1d24ef4cc47fb3b45e19529a1e164d5295fa56c0978a093967891a43bb2223d88009d1aa5de3b706f1b21feff69f5bb484bd1568abb'
