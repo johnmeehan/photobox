c753faca64feb3b6b800c4080a379fe037ac12d3
John Meehan <john.meehan1@gmail.com>
2014-06-04 21:49:04 +0100

    aligned nav bar and header

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Picture, :type => :model do
   it { should have_property :title }
   it { should have_property :description }
-  it { should have_property :image }
+  it { should have_property :file }
 
 end
