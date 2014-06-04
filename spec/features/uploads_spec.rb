72bbeffadf541c408e7c9f803e3c0f2d1a5a63a5
John Meehan <john.meehan1@gmail.com>
2014-06-04 22:19:52 +0100

    CSS:  added alert box notifications

--- spec/features/uploads_spec.rb
+++ spec/features/uploads_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "Uploads", :type => :feature do
   describe "Uploading pictures" do 
   	visit root_path
-  	click_link 'New Picture'
+  	click_link 'Upload'
   	fill_in "Title", with: "My Photo"
   	fill_in "Description", with: "Holidays"
   	click_button "Save"
