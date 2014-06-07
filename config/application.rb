fd96d70768410e3d329bbc6689d091d136e85637
John Meehan <john.meehan1@gmail.com>
2014-06-07 21:31:50 +0100

    converted rvmrc to ruby-version

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require 'action_mailer/railtie'
 require 'active_resource/railtie'
 require 'rails/test_unit/railtie'
 require "will_paginate/data_mapper"
-# require 'elasticsearch/model'
+require 'elasticsearch/model'
 # require 'tire'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
